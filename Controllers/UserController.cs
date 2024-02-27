8a7749fdb31f217c9cd63e8849685ee72a22b5a6
Vikas ghodke <vikasghodke468@gmail.com>
2024-02-27 18:03:29 +0530

    First Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using BusinessLayer.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
+using RepoLayer.Entiry;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace EntiryFrameworkTstProject1.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBL userBL;
+
+        public UserController(IUserBL userBL)
+        {
+            this.userBL = userBL;
+        }
+        [HttpPost]
+        
+        public IActionResult AddUser(UserModel userModel)
+        {
+            var result = userBL.AddUserDetails(userModel);
+            if (result != null)
+            {
+                return Ok(new { Success = true, Message = "added sucessfully", Data = result });
+            }
+            else
+            {
+                return BadRequest(new { Success = false, Message = "something wet wrong" });
+            }
+        }
+        [HttpGet]
+        public IActionResult ViewUserDetail()
+        {
+            IEnumerable<UserEntiry> entity = userBL.ViewUserDetail();
+            return Ok(entity);
+
+           
+
+
+        }
+
+        
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+using BusinessLayer.Interface;
+using BusinessLayer.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RepoLayer.Context;
+using RepoLayer.Interface;
+using RepoLayer.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntiryFrameworkTstProject1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<FundooNoteContext>(x => x.UseSqlServer(Configuration["ConnectionStrings:FundooDB"]));
+            services.AddTransient<IUserRL, UserService>();
+            services.AddTransient<IUserBL, UserBL>();
+            
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
